336d8f09dee3d5a88f8cd3a0a568601442460883
bhavya11720-art <bhavya.11720@gmail.com>
2025-12-17 14:10:11 +0530

    Fix model loading and import statements

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import JOBLIB
+import pickle
 import numpy as np
 
 # Load the trained model
-#with open('model.pkl', 'rb') as file:
-model = JOBLIB.load(file)
+with open('model.pkl', 'rb') as file:
+    model = JOBLIB.load(file)
 
 # App title
 st.title("🩺 Logistic Regression Diabetes Prediction App")
@@ -35,5 +35,6 @@ if st.button("Predict"):
         st.error("The person is **diabetic**")
     else:
         st.success("The person is **not diabetic**")
+
 
 
